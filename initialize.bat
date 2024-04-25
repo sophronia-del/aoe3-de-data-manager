be670e91a7409226a67fd77adcb1b50d1db8c120
sophronia <sophronia.del@gmail.com>
2024-04-25 20:48:05 +0800

    fix up path to git hook script

--- initialize.bat
+++ initialize.bat
@@ -1,4 +1,4 @@
 del /f /q ".git\hooks\post-commit" 2>nul
 del /f /q ".git\hooks\post-merge" 2>nul
-mklink ".git\hooks\post-commit" "scripts\post-commit-or-merge.sh"
-mklink ".git\hooks\post-merge" "scripts\post-commit-or-merge.sh"
+mklink ".git\hooks\post-commit" "..\..\scripts\post-commit-or-merge.sh"
+mklink ".git\hooks\post-merge" "..\..\scripts\post-commit-or-merge.sh"
